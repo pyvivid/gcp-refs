8cb6cc8155e1b6cc6c409866a86f4afc66b56839
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-02-15 19:45:59 +0530

    Delete startup_ubuntu.sh

--- startup_ubuntu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# if the startup script fails add sudo as prefix each line
-apt update 
-apt -y install apache2
-echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
