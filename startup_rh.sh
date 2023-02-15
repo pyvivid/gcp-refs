a9347b7b6d7c1a0cf7d773aedc46b5d6efcf971f
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-02-15 08:45:27 +0530

    Update startup_rh.sh

--- startup_rh.sh
+++ startup_rh.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # if the startup script fails add sudo as prefix each line
 yum update -y
+reboot
 yum -y install httpd
+systemctl start httpd
+systemctl enable httpd
 echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
