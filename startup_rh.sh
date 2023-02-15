19f976d829e3eb7a6d87e7bf37d4807b4f9cdb91
PyV!V!D <94853400+pyvivid@users.noreply.github.com>
2023-02-15 19:45:51 +0530

    Delete startup_rh.sh

--- startup_rh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# if the startup script fails add sudo as prefix each line
-yum update -y
-reboot
-yum -y install httpd
-systemctl start httpd
-systemctl enable httpd
-echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
